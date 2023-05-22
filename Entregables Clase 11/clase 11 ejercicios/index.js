569393c9b738f3ef98bd92a4a7785d50ea907932
Ramiro Tapias Broder <ramirotapiasbroder@yahoo.com.ar>
2023-05-22 18:43:02 -0300

    actualizacion clase 11

---
+++ Entregables Clase 11/clase 11 ejercicios/index.js
@@ -0,0 +1,14 @@
+let nombre="Ramiro";
+let apellido="Tapias Broder";
+
+console.log(nombre+" "+apellido);
+
+let num1=10;
+let num2=15;
+let suma=num1+num2;
+console.log(suma);
+
+
+let numx= 25
+let resultado=("suma",numx);
+console.log(suma+numx);
