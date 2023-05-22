0c4c6ae78685ad2986df0a6cb09175d6eab2e1c3
Ramiro Tapias Broder <ramirotapiasbroder@yahoo.com.ar>
2023-05-22 19:27:47 -0300

    Update index.js

--- Entregables Clase 10/Tarea/index.js
+++ Entregables Clase 10/Tarea/index.js
@@ -12,3 +12,58 @@ console.log(suma);
 let numx= 25
 let resultado=("suma",numx);
 console.log(suma+numx);
+
+//*SI (mayorEdad >=18){…...}SINO{...}//*
+
+const mayorEdad=18;
+if(mayorEdad >= 18){
+    console.log("Es mayor de edad");
+}else{
+    console.lo("Es menor de edad");
+}
+
+//*ANIDACION SI (CONDICION){}SINO SI...//*
+
+let nota= 3;
+if (nota < 4){
+    console.log (’aplazado’);
+} else if (nota < 7) {
+console.log(’Desaprobado’);
+} else if (nota < 9) {
+console.log(’Aprobado’);
+} else {
+console.log(’Sobresaliente’);
+}
+ //*Switch Case//*
+ let expr= "Adios";
+switch (expr) (
+case 'Hola' :
+comsole.log ("Buenos Dias");
+break;
+case 'Adios'
+consolo.log("hasta Pronto");
+break;
+case 'Ayuda":
+console. log ("En que te puedo Ayudar?");
+break
+default:
+console. log("Disculpa no te entiendo");
+}
+
+let ejercicio = "Lunes"
+switch (ejercicio) {
+case "Lunes"
+console.log("Caminata de 5 km");
+break;
+case "Martes":
+console.log("Caminata de 10 km");
+break;
+case "Miercoles" :
+console.log("Correr 5 km");
+break;
+case "Viernes":
+console.log("correr 10 km")
+break;
+defeult:
+console.log("Descanso");
+}
