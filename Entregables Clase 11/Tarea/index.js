c6b7e28d272956a2f2aed74e76d9f550dadfce09
Ramiro Tapias Broder <ramirotapiasbroder@yahoo.com.ar>
2023-05-23 03:33:50 -0300

    Update index.js

--- Entregables Clase 11/Tarea/index.js
+++ Entregables Clase 11/Tarea/index.js
@@ -1,17 +1,14 @@
 let helado=150;
-let Oreo=10;
-let KitKat=15;
-let Kinder=25;
 let topping=[Oreo,KitKat,Kinder];
-let precio=topping
+let precio=[10,15,25]
 let precioFinal=precio+helado;
 
 if(helado+Oreo){
-    console.log("El helado cuesta $")+precioFinal;
+    console.log("El helado cuesta $ ")+precioFinal;
 }else if(helado+KitKat){
-    console.log("El helado cuesta $")+precioFinal;
+    console.log("El helado cuesta $ ")+precioFinal;
 }else if(helado+Kinder){
-    console.log("El helado cuesta $")+precioFinal;
+    console.log("El helado cuesta $ ")+precioFinal;
 }else{
     console.log("No tenemos ese topping");
 }
