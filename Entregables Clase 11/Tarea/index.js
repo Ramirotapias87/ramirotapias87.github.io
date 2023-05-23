04900d9ba487881f7ac3ab27fd64ab3c3c5147d1
Ramiro Tapias Broder <ramirotapiasbroder@yahoo.com.ar>
2023-05-23 03:28:53 -0300

    Update index.js

--- Entregables Clase 11/Tarea/index.js
+++ Entregables Clase 11/Tarea/index.js
@@ -1,5 +1,8 @@
 let helado=150;
-let topping=[Oreo(10),KitKat(15),Kinder(25)];
+let topping=[Oreo,KitKat,Kinder];
+let Oreo=10;
+let KitKat=15;
+let Kinder=25;
 let precio=topping
 let precioFinal=precio+helado;
 
