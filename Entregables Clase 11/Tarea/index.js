bd6a95151fab722cf888e23aea91ba68f8652d05
Ramiro Tapias Broder <ramirotapiasbroder@yahoo.com.ar>
2023-05-23 03:44:03 -0300

    Update index.js

--- Entregables Clase 11/Tarea/index.js
+++ Entregables Clase 11/Tarea/index.js
@@ -8,11 +8,11 @@ let precioFinal=precio+helado;
 
 
 if(helado+Oreo){
-    console.log("El helado cuesta $ ")+precioFinal;
+    console.log("El helado cuesta $ "+precioFinal);
 }else if(helado+KitKat){
-    console.log("El helado cuesta $ ")+precioFinal;
+    console.log("El helado cuesta $ "+precioFinal);
 }else if(helado+Kinder){
-    console.log("El helado cuesta $ ")+precioFinal;
+    console.log("El helado cuesta $ "+precioFinal);
 }else{
     console.log("No tenemos ese topping");
 }
