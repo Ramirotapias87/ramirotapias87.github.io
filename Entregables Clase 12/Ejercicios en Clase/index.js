0c14b8169870777dabd892ed4623faea90601ee9
Ramirotapiasbroder.github.io <ramirotapiasbroder@yahoo.com.ar>
2023-05-24 20:00:20 -0300

    Update index.js

--- Entregables Clase 12/Ejercicios en Clase/index.js
+++ Entregables Clase 12/Ejercicios en Clase/index.js
@@ -42,3 +42,24 @@ function calcularPrecioTotal(n1,n2){
 sumar(100,100);
 
 //*template literal*//
+
+//* llamar a la funtion se llama invocar*//
+
+const perdirYsumar=(n1,n2)=>{
+    let n1 = prompt("ingrese un numero");
+    let n2 = prompt("Ingrese otro numero");
+    let resultado = parseInt(n1)+ parseInt(n2);
+    alert(el restado de ${n1}+${n2});
+}
+
+
+function calcularPrecioTotal(precio){
+    var impuestos =1.16;
+    var gatosEnvio=10;
+    var precioTotal= (precio * impuestos) + gastosEnvio;
+    alert("Su total a pagar: " + precioTotal); 
+}
+
+// llamada a la funcion
+
+calcularPrecioTotal(50);
