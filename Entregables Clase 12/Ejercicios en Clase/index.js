25891806cddedf0660d97965a10b992d81a150df
Ramirotapiasbroder.github.io <ramirotapiasbroder@yahoo.com.ar>
2023-05-24 19:47:29 -0300

    Update index.js

--- Entregables Clase 12/Ejercicios en Clase/index.js
+++ Entregables Clase 12/Ejercicios en Clase/index.js
@@ -2,3 +2,43 @@ function saludo(persona) {
     console.log("Hola "+ persona);
 }
 saludo("Ramiro");
+
+
+
+function sumar2(a+b) {
+    console.log(a+b);
+}
+sumar(a+b)
+
+
+function sumar(n1,n2) {
+    console.log(n1+n2);
+}
+sumar(3,2)
+
+
+//* arrow function *//
+
+ //* FUNCION BASE -> function sumar(a,b){..} *//
+
+//* ARROW FUNCTION -> const sumar=(a,b)=>{..} *//
+
+
+var resultado
+var num1=3;
+var num2=5;
+function suma_y_muestra() {
+    resultado=num1+num2;
+    alert("El resultado es" + resultado);   
+}
+suma_y_muestra();
+
+
+function calcularPrecioTotal(n1,n2){
+    let precio =n1+n2;
+    alert(`El resutado de ${n1} +${n2} es: ${resultado}`);
+    
+}
+sumar(100,100);
+
+//*template literal*//
