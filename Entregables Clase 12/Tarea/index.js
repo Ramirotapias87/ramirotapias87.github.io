e29a77a82975ad17a24a8beaee128a040abc5e8e
Ramiro Tapias Broder <ramirotapiasbroder@yahoo.com.ar>
2023-05-28 08:57:42 -0300

    Update index.js

--- Entregables Clase 12/Tarea/index.js
+++ Entregables Clase 12/Tarea/index.js
@@ -1,4 +1,4 @@
-function saludo(nombre) {
-   let name= prompt("Ingrese su Nombre");
-   alert("Bienvenido ")+nombre;
-}
+ 
+  let saludo= prompt("Ingrese su Nombre");
+    alert (`Bienvenido ${saludo}`);
+    console.log();
